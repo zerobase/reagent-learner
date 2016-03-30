f8224b84f3630bcfc183a3bc0f884a515c75f7ed
Hideto Ishibashi <hideto@zerobase.jp>
2016-03-30 16:13:38 +0900

    Add components from Reagent tutorial
    http://reagent-project.github.io/

--- env/dev/cljs/reagent_learner/cards.cljs
+++ env/dev/cljs/reagent_learner/cards.cljs
@@ -13,6 +13,30 @@
 (defcard-rg home-page-card
   [core/home-page])
 
+(defcard-rg simple-component
+  [core/simple-component])
+
+(defcard-rg simple-parent
+  [core/simple-parent])
+
+(defcard-rg say-hello
+  [core/say-hello])
+
+(defcard-rg say-foo
+  [core/hello-component "foo"])
+
+(defcard-rg lister
+  [core/lister-user])
+
+(defcard-rg counter
+  [core/counting-component])
+
+(defcard-rg timer
+  [core/timer-component])
+
+(defcard-rg shared-state
+  [core/shared-state])
+
 (reagent/render [:div] (.getElementById js/document "app"))
 
 ;; remember to run 'lein figwheel devcards' and then browse to
--- src/cljs/reagent_learner/core.cljs
+++ src/cljs/reagent_learner/core.cljs
@@ -5,11 +5,75 @@
               [accountant.core :as accountant]))
 
 ;; -------------------------
+;; Components
+
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(defn simple-parent []
+  [:div
+   [:p "I include simple-component."]
+   [simple-component]])
+
+(defn hello-component [name]
+  [:p "Hello, " name "!"])
+
+(defn say-hello []
+  [hello-component "world"])
+
+(defn lister [items]
+  [:ul
+   (for [item items]
+     ^{:key item} [:li "Item " item])])
+
+(defn lister-user []
+  [:div
+   "Here is a list:"
+   [lister (range 3)]])
+
+(defonce click-count (reagent/atom 0))
+
+(defn counting-component []
+  [:div
+   "The atom " [:code "click-count"] " has value: "
+   @click-count ". "
+   [:input {:type "button" :value "Click me!"
+            :on-click #(swap! click-count inc)}]])
+
+(defn timer-component []
+  (let [seconds-elapsed (reagent/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! seconds-elapsed inc) 1000)
+      [:div
+       "Seconds Elapsed: " @seconds-elapsed])))
+
+(defn atom-input [value]
+  [:input {:type "text"
+           :value @value
+           :on-change #(reset! value (-> % .-target .-value))}])
+
+(defn shared-state []
+  (let [val (reagent/atom "foo")]
+    (fn []
+      [:div
+       [:p "The value is now: " @val]
+       [:p "Change it here: " [atom-input val]]])))
+
+;; -------------------------
 ;; Views
 
 (defn home-page []
   [:div [:h2 "Welcome to reagent-learner"]
-   [:div [:a {:href "/about"} "go to about page"]]])
+   [:div [:a {:href "/about"} "go to about page"]]
+   [simple-parent]
+   [say-hello]
+   [lister-user]
+   [counting-component]
+   [timer-component]])
 
 (defn about-page []
   [:div [:h2 "About reagent-learner"]
